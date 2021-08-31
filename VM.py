25d403889e4a47d38b9989ab4465d2ee4fd38396
mizu-bai <shiragawa4519@outlook.com>
2021-08-31 11:51:54 +0800

    Update

---
+++ VM.py
@@ -0,0 +1,98 @@
+class SimpleVM():
+    def __init__(self):
+        self.stack = []
+        self.operations = {
+            "PUSH": self.op_push,
+            "POP":  self.op_pop,
+            "ADD":  self.op_add,
+            "SUB":  self.op_sub,
+            "MUL":  self.op_mul,
+            "DIV":  self.op_div,
+            "EQ":   self.op_eq,
+            "GT":   self.op_gt,
+            "LT":   self.op_lt,
+            "AND":  self.op_and,
+            "OR":   self.op_or,
+            "NOT":  self.op_not,
+            "XOR":  self.op_xor
+        }
+        self.code = []
+
+    def op_push(self, x):
+        self.stack.append(x)
+
+    def op_pop(self):
+        return self.stack.pop()
+
+    def op_add(self):
+        t1 = self.stack.pop()
+        t2 = self.stack.pop()
+        self.op_push(t2 + t1)
+
+    def op_sub(self):
+        t1 = self.stack.pop()
+        t2 = self.stack.pop()
+        self.op_push(t2 - t1)
+
+    def op_mul(self):
+        t1 = self.stack.pop()
+        t2 = self.stack.pop()
+        self.op_push(t2 * t1)
+
+    def op_div(self):
+        t1 = self.stack.pop()
+        t2 = self.stack.pop()
+        self.op_push(t2 // t1)
+
+    def op_eq(self):
+        t1 = self.stack.pop()
+        t2 = self.stack.pop()
+        self.op_push(1 if t2 == t1 else 0)
+        
+    def op_gt(self):
+        t1 = self.stack.pop()
+        t2 = self.stack.pop()
+        self.op_push(1 if t2 > t1 else 0)
+
+    def op_lt(self):
+        t1 = self.stack.pop()
+        t2 = self.stack.pop()
+        self.op_push(1 if t2 < t1 else 0)
+
+    def op_and(self):
+        t1 = self.stack.pop()
+        t2 = self.stack.pop()
+        self.op_push(t2 & t1)
+
+    def op_or(self):
+        t1 = self.stack.pop()
+        t2 = self.stack.pop()
+        self.op_push(t2 | t1)
+
+    def op_not(self):
+        t1 = self.stack.pop()
+        self.op_push(~t1)
+
+    def op_xor(self):
+        t1 = self.stack.pop()
+        t2 = self.stack.pop()
+        self.op_push(t2 ^ t1)
+
+    def run(self):
+        def shift(l):
+            t = l[0]
+            l = l[1:]
+            return t, l
+        
+        while self.code:
+            t, self.code = shift(self.code)
+            if t == "PUSH":
+                t2, self.code = shift(self.code)
+                self.operations[t](t2)
+            elif t == "POP":
+                return self.operations[t]()
+            else:
+                self.operations[t]()
+
+        return None
+        
